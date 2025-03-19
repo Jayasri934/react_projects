b0a41de156d7d8d50d86badbef975b557148dc8a
Jayasri934 <elurijayasri96@gmail.com>
2025-03-19 10:30:10 +0530

    ok

---
+++ src/App1.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function App1(){
+    return(
+        <div style={{backgroundColor:'yellow'}}>App1</div>
+    )
+}
---
+++ src/components/App5.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useState} from "react";
+import "./App5.css";
+export default function App5() {
+    <div>
+<input type="text"></input>
+<button>ADD</button>      
+    </div>
+  )
+}
